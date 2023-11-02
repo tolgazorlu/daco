3632d2579142a45338a162856d189e78da1414e3
tolgazorlu <tolgazorlu17@gmail.com>
2023-11-02 23:20:51 +0300

    problem delete modal fixed

--- client/src/components/Dashboard/ProblemsTable.tsx
+++ client/src/components/Dashboard/ProblemsTable.tsx
@@ -91,18 +91,18 @@ const ProblemsTable = () => {
                     <div className="modal-box">
                       <h3 className="font-bold text-lg">Attention!</h3>
                       <p className="py-4">
-                        Do you want to delete this product? You can not take it
+                        Do you want to delete this problem? You can not take it
                         back the process!
                       </p>
                       <div className="modal-action">
                         <form className="flex">
                           <button
-                            className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
+                            className="btn btn-error mr-2 text-error-content"
                             onClick={() => deleteProblemHandler(problemId)}
                           >
                             Delete
                           </button>
-                          <button className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
+                          <button className="btn">
                             Close
                           </button>
                         </form>
