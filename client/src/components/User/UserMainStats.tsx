d4402c2aa4c0e25b923099f881b74435197a5788
tolgazorlu <tolgazorlu17@gmail.com>
2023-11-09 00:23:32 +0300

    user main stats added

--- client/src/components/User/UserMainStats.tsx
+++ client/src/components/User/UserMainStats.tsx
@@ -1,30 +1,42 @@
 /* eslint-disable prefer-const */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+import { useContext, useEffect, useState } from "react";
+import { User } from "../../contexts/User";
+import { useGetDailyProblemsQuery } from "../../hooks/problemHooks";
+
 const UserMainStats = () => {
+  const { state } = useContext(User);
+  const { userInfo } = state;
+  const { data: problems } = useGetDailyProblemsQuery();
+  const [day, setDay] = useState<number>()
+  
+  useEffect(() => {
+    if(problems){
+      setDay(problems[0].day)
+    }
+  }, [problems, day])
+
   return (
     <div className="stats bg-primary shadow-md">
       <div className="stat">
         <div className="stat-title text-primary-content">Solved Problems</div>
-        <div className="stat-value text-primary-content">25</div>
+        <div className="stat-value text-primary-content">
+          {userInfo?.solvedProblems.length}
+        </div>
         <div className="stat-actions">
-          <button
-            className="btn btn-sm btn-primary-content text-primary hover:text-primary/50"
-          >
+          <a href="/" className="btn btn-sm btn-primary-content text-primary">
             Check New Problems
-          </button>
+          </a>
         </div>
       </div>
 
       <div className="stat">
         <div className="stat-title text-primary-content">Total Day</div>
-        <div className="stat-value text-primary-content">123</div>
+        <div className="stat-value text-primary-content">{day}</div>
         <div className="stat-actions flex gap-2">
-          <button className="btn btn-sm btn-primary-content text-primary hover:text-primary/50">
+          <button className="btn btn-sm btn-nautral-content text-nautral">
             Check Statistics
-          </button>
-          <button className="btn btn-sm btn-primary-content text-primary hover:text-primary/50">
-            Copy Link
           </button>
         </div>
       </div>
