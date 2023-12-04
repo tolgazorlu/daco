579ee0dfdd7ba209009df9f6a587d3ebd88c00ff
tolgazorlu <tolgazorlu17@gmail.com>
2023-12-04 18:15:32 +0300

    type safe added

--- server/src/utils/dailySchedule.ts
+++ server/src/utils/dailySchedule.ts
@@ -1,18 +1,18 @@
 const schedule = require("node-schedule");
 
-let day = 1;
-const date = new Date();
-const monthDate = date.toLocaleString("default", { month: "long" });
-const dayDate = date.toLocaleString("default", { day: "numeric" });
-const yearDate = date.toLocaleString("default", { year: "numeric" });
-let todaysDate = `${monthDate}, ${dayDate}, ${yearDate}`;
+let day: number = 1;
+const date: Date = new Date();
+const monthDate: string = date.toLocaleString("default", { month: "long" });
+const dayDate: string = date.toLocaleString("default", { day: "numeric" });
+const yearDate: string = date.toLocaleString("default", { year: "numeric" });
+let todaysDate: string = `${monthDate}, ${dayDate}, ${yearDate}`;
 
 const Job = schedule.scheduleJob("0 0 * * *", function () {
   day++;
-  const date = new Date();
-  const monthDate = date.toLocaleString("default", { month: "long" });
-  const dayDate = date.toLocaleString("default", { day: "numeric" });
-  const yearDate = date.toLocaleString("default", { year: "numeric" });
+  const date: Date = new Date();
+  const monthDate: string = date.toLocaleString("default", { month: "long" });
+  const dayDate: string = date.toLocaleString("default", { day: "numeric" });
+  const yearDate: string = date.toLocaleString("default", { year: "numeric" });
   todaysDate = `${monthDate}, ${dayDate}, ${yearDate}`;
 });
 
