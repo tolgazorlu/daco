48ba98a3a1c11a2cd3a03799f5c5cf557e054c45
Tolga Zorlu <tolgazorlu@Tolga-Air.home>
2024-02-23 10:09:06 +0300

    comment lines removed

--- client/src/layouts/Sidebar.tsx
+++ client/src/layouts/Sidebar.tsx
@@ -207,24 +207,6 @@ const Sidebar = (props: { isOpen: boolean }) => {
               <span className="ml-4">Support the Project</span>
             </a>
           </li>
-          {/* <li>
-            <a
-              href="https://github.com/tolgazorlu/daco"
-              target="_blank"
-              className="flex items-center p-2 transition duration-75 rounded-lg hover:bg-primary/50"
-            >
-              <svg
-                className="flex-shrink-0 w-5 h-5  transition duration-75 "
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 16 20"
-              >
-                <path d="M16 14V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v15a3 3 0 0 0 3 3h12a1 1 0 0 0 0-2h-1v-2a2 2 0 0 0 2-2ZM4 2h2v12H4V2Zm8 16H3a1 1 0 0 1 0-2h9v2Z" />
-              </svg>
-              <span className="ml-3">Project</span>
-            </a>
-          </li> */}
           <li>
             <a
               href="https://daisyui.com/docs/themes/"
