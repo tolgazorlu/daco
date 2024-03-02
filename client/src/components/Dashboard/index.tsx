e9d6ff12ed578c04ac5da45077120fa6708b3d5e
Tolga Zorlu <tolgazorlu@Tolga-Air.home>
2024-03-02 18:32:24 +0300

    admin dashboard page responive design fixed, new stats added

--- client/src/components/Dashboard/index.tsx
+++ client/src/components/Dashboard/index.tsx
@@ -3,23 +3,21 @@
 import { Helmet } from "react-helmet-async";
 import DailyProblemsTable from "./DailyProblemsTable";
 import MainStats from "./MainStats";
-import MiniStats from "./MiniStats";
 import Layout from "../Layouts";
 
 const Dashboard = () => {
-  return (
-    <>
-      <Helmet>
-        <title>Admin cockpit for Dashboard</title>
-      </Helmet>
-      <Layout />
-      <div className="px-4 py-8 sm:ml-64 mt-14 flex flex-col gap-4">
-        <MainStats />
-        <MiniStats />
-        <DailyProblemsTable />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Helmet>
+                <title>Admin cockpit for Dashboard</title>
+            </Helmet>
+            <Layout />
+            <div className="px-4 lg:px-32 py-8 sm:ml-64 mt-14 flex flex-col gap-4">
+                <MainStats />
+                <DailyProblemsTable />
+            </div>
+        </>
+    );
 };
 
 export default Dashboard;
