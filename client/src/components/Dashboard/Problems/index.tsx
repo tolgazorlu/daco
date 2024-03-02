9e19c08e42ac5b1114050d3d32bbdb3502942374
Tolga Zorlu <tolgazorlu@Tolga-Air.home>
2024-03-02 18:30:42 +0300

    dashboard problems page responsive design fixed

--- client/src/components/Dashboard/Problems/index.tsx
+++ client/src/components/Dashboard/Problems/index.tsx
@@ -6,31 +6,34 @@ import Layout from "../../Layouts";
 import { Helmet } from "react-helmet-async";
 
 const Problems = () => {
-  return (
-    <>
-      <Helmet>
-        <title>Admin cockpit for Problems</title>
-      </Helmet>
-      <Layout />
-      <div className="px-4 py-8 sm:ml-64 mt-16 flex flex-col gap-4">
-        <div className="p-2 rounded-lg">
-          <div className="w-full h-10">
-            <button
-              className="btn btn-sm btn-primary text-primary-content hover:text-primary/50 float-right"
-              onClick={() => {
-                let el: any = document.getElementById("add-problem-modal")!;
-                el.showModal();
-              }}
-            >
-              Add Problem
-            </button>
-            <AddProblemModal />
-          </div>
-          <ProblemsTable />
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Helmet>
+                <title>Admin cockpit for Problems</title>
+            </Helmet>
+            <Layout />
+            <div className="px-4 lg:px-32 py-8 sm:ml-64 mt-16 flex flex-col gap-4">
+                <div className="p-2 rounded-lg">
+                    <div className="w-full h-10">
+                        <button
+                            className="btn btn-sm btn-primary text-primary-content hover:text-primary/50 float-right"
+                            onClick={() => {
+                                let el: any =
+                                    document.getElementById(
+                                        "add-problem-modal",
+                                    )!;
+                                el.showModal();
+                            }}
+                        >
+                            Add Problem
+                        </button>
+                        <AddProblemModal />
+                    </div>
+                    <ProblemsTable />
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Problems;
