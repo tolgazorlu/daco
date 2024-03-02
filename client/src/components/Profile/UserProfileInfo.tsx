093a820ba0807506dab08b2f0cabae784e679cfa
Tolga Zorlu <tolgazorlu@Tolga-Air.home>
2024-03-02 22:04:44 +0300

    user info widget changed

--- client/src/components/Profile/UserProfileInfo.tsx
+++ client/src/components/Profile/UserProfileInfo.tsx
@@ -34,9 +34,27 @@ const UserProfileInfo = () => {
                             />
                         </div>
                         <div className="stat">
-                            <div className="stat-value text-base">Username</div>
-                            <div className="stat-title text-primary">
-                                {userInfo?.username}
+                            <div className="stat-figure text-primary">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    className="inline-block w-8 h-8 stroke-current"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Zm6-10.125a1.875 1.875 0 1 1-3.75 0 1.875 1.875 0 0 1 3.75 0Zm1.294 6.336a6.721 6.721 0 0 1-3.17.789 6.721 6.721 0 0 1-3.168-.789 3.376 3.376 0 0 1 6.338 0Z"
+                                    />
+                                </svg>
+                            </div>
+                            <div className="stat-title">Username</div>
+                            <div className="stat-value text-primary">
+                                {userInfo.username}
+                            </div>
+                            <div className="stat-desc">
+                                You can change profile information here!
                             </div>
                             <div className="stat-actions flex gap-2">
                                 <button
@@ -67,9 +85,10 @@ const UserProfileInfo = () => {
                             </div>
                         </div>
                     </div>
+
                     <div className="stats shadow-lg border border-base-300">
                         <div className="stat  shadow-lg">
-                            <div className="stat-figure text-primary">
+                            <div className="stat-figure text-accent">
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     fill="none"
@@ -85,14 +104,14 @@ const UserProfileInfo = () => {
                                 </svg>
                             </div>
                             <div className="stat-title">You're in</div>
-                            <div className="stat-value text-primary">
+                            <div className="stat-value text-accent">
                                 {day}. Day
                             </div>
                             <div className="stat-desc">You're amazing!</div>
                         </div>
 
                         <div className="stat">
-                            <div className="stat-figure text-secondary">
+                            <div className="stat-figure text-accent">
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     fill="none"
@@ -108,7 +127,7 @@ const UserProfileInfo = () => {
                                 </svg>
                             </div>
                             <div className="stat-title">Created At</div>
-                            <div className="stat-value text-secondary">
+                            <div className="stat-value text-accent">
                                 {registerAt}
                             </div>
                             <div className="stat-desc">
