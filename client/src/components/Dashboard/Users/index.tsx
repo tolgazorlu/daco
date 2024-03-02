50378b2c52ffd3bbe3f23c254958fb24c1dafa6e
Tolga Zorlu <tolgazorlu@Tolga-Air.home>
2024-03-02 18:31:42 +0300

    dashboard users page responsive design fixed

--- client/src/components/Dashboard/Users/index.tsx
+++ client/src/components/Dashboard/Users/index.tsx
@@ -3,17 +3,17 @@ import UsersTable from "../UsersTable";
 import Layout from "../../Layouts";
 
 const Users = () => {
-  return (
-    <>
-      <Helmet>
-        <title>Admin cockpit for Users</title>
-      </Helmet>
-      <Layout />
-      <div className="p-4 sm:ml-64 mt-16 flex flex-col gap-4">
-        <UsersTable />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Helmet>
+                <title>Admin cockpit for Users</title>
+            </Helmet>
+            <Layout />
+            <div className="p-4 lg:px-32 sm:ml-64 mt-16 flex flex-col gap-4">
+                <UsersTable />
+            </div>
+        </>
+    );
 };
 
 export default Users;
