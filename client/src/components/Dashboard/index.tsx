a09d8b63fb86e21d423eb5c7d986f05be21bdaa1
Tolga Zorlu <tolgazorlu@Tolga-Air.mshome.net>
2024-03-03 16:46:07 +0300

    contact table widget added

--- client/src/components/Dashboard/index.tsx
+++ client/src/components/Dashboard/index.tsx
@@ -4,6 +4,7 @@ import { Helmet } from "react-helmet-async";
 import DailyProblemsTable from "./DailyProblemsTable";
 import MainStats from "./MainStats";
 import Layout from "../Layouts";
+import ContactTable from "./ContactTable";
 
 const Dashboard = () => {
     return (
@@ -12,9 +13,10 @@ const Dashboard = () => {
                 <title>Admin cockpit for Dashboard</title>
             </Helmet>
             <Layout />
-            <div className="px-4 lg:px-32 py-8 sm:ml-64 mt-14 flex flex-col gap-4">
+            <div className="px-4 py-8 lg:px-32 sm:ml-64 mt-14 flex flex-col gap-4">
                 <MainStats />
                 <DailyProblemsTable />
+                <ContactTable />
             </div>
         </>
     );
