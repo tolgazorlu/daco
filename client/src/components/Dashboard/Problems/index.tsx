88a05c391c8102eeaee46fa97d1fd5ce6536c7a5
Tolga Zorlu <tolgazorlu@Tolga-Air.mshome.net>
2024-03-03 16:44:10 +0300

    Dashboard problems page fixed

--- client/src/components/Dashboard/Problems/index.tsx
+++ client/src/components/Dashboard/Problems/index.tsx
@@ -12,25 +12,19 @@ const Problems = () => {
                 <title>Admin cockpit for Problems</title>
             </Helmet>
             <Layout />
-            <div className="px-4 lg:px-32 py-8 sm:ml-64 mt-16 flex flex-col gap-4">
-                <div className="p-2 rounded-lg">
-                    <div className="w-full h-10">
-                        <button
-                            className="btn btn-sm btn-primary text-primary-content hover:text-primary/50 float-right"
-                            onClick={() => {
-                                let el: any =
-                                    document.getElementById(
-                                        "add-problem-modal",
-                                    )!;
-                                el.showModal();
-                            }}
-                        >
-                            Add Problem
-                        </button>
-                        <AddProblemModal />
-                    </div>
-                    <ProblemsTable />
-                </div>
+            <div className="px-4 py-8 lg:px-32 sm:ml-64 mt-14 flex flex-col gap-4">
+                <ProblemsTable />
+                <button
+                    className="btn btn-sm btn-primary text-primary-content hover:text-primary/50"
+                    onClick={() => {
+                        let el: any =
+                            document.getElementById("add-problem-modal")!;
+                        el.showModal();
+                    }}
+                >
+                    Add Problem
+                </button>
+                <AddProblemModal />
             </div>
         </>
     );
