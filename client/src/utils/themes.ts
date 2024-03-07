a90d0ea0be9dc45fde393f161b4b2039e27d2364
Tolga Zorlu <tolgazorlu@Tolga-Air.home>
2024-03-07 20:29:19 +0300

    themes removed except light and dark

--- client/src/utils/themes.ts
+++ client/src/utils/themes.ts
@@ -1,38 +1,5 @@
 export type themes = string;
 
-const themes: themes[] = [
-  "light",
-  "dark",
-  "cupcake",
-  "bumblebee",
-  "emerald",
-  "corporate",
-  "synthwave",
-  "retro",
-  "cyberpunk",
-  "valentine",
-  "halloween",
-  "garden",
-  "forest",
-  "aqua",
-  "lofi",
-  "pastel",
-  "fantasy",
-  "wireframe",
-  "black",
-  "luxury",
-  "dracula",
-  "cmyk",
-  "autumn",
-  "business",
-  "acid",
-  "lemonade",
-  "night",
-  "coffee",
-  "winter",
-  "dim",
-  "nord",
-  "sunset",
-];
+const themes: themes[] = ["light", "dark"];
 
 export default themes;
