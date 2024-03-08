a56e42403c499ecd81b4db23edc41ea119a0256b
Tolga Zorlu <tolgazorlu@Tolga-Air.home>
2024-03-08 19:28:45 +0300

    svg changed

--- client/src/components/Banner/index.tsx
+++ client/src/components/Banner/index.tsx
@@ -7,6 +7,30 @@ const index = () => {
             <Layout />
             <section className="lg:grid grid-cols-2 px-8 lg:px-32">
                 <div className="h-screen md:snap-start flex flex-col justify-center">
+                    <a
+                        href="#"
+                        className="bg-neutral text-neutral-content inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm  rounded-full w-2/3"
+                        role="alert"
+                    >
+                        <span className="text-xs bg-primary text-primary-content rounded-full px-4 py-1.5 mr-3">
+                            Discord
+                        </span>{" "}
+                        <span className="text-sm font-medium">
+                            Let's join the server!
+                        </span>
+                        <svg
+                            className="ml-2 w-5 h-5"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                clip-rule="evenodd"
+                            ></path>
+                        </svg>
+                    </a>
                     <span className="text-9xl font-bandal font-bold text-primary">
                         daco
                     </span>
@@ -62,7 +86,7 @@ const index = () => {
                         </a>
                     </div>
                 </div>
-                <div className="hidden md:h-screen lg:flex flex-col justify-center">
+                <div className="hidden md:h-screen lg:flex flex-col justify-center p-6">
                     <img src={Image} />
                 </div>
             </section>
