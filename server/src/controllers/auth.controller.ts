ea94ff6e9466d67c4effbf6022d4794a3883dc6d
Tolga Zorlu <tolgazorlu@Tolga-Air.home>
2024-03-08 18:33:19 +0300

    email links fixed

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -67,7 +67,7 @@ module.exports.Register = async (
       emailToken,
     );
 
-    const link = `https://daco.space/${newUser._id}/verify/${emailToken}`;
+    const link = `${process.env.EMAIL_LINK}${newUser._id}/verify/${emailToken}`;
 
     verifyEmail(email, username, link);
 
@@ -275,7 +275,7 @@ module.exports.ForgotPassword = async (req: Request, res: Response) => {
       user.resetPasswordExpires = new Date(Date.now() + 3600000); // 1 hour
       await user.save();
 
-      const resetURL = `https://daco.space/reset-password/${token}`;
+      const resetURL = `${process.env.EMAIL_LINK}reset-password/${token}`;
 
       forgotPasswordEmail(email, resetURL);
   } catch (error) {
