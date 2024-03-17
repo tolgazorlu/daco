067c10cac3ae62ab15160f00f76c04c3323d067a
Tolga Zorlu <tolgazorlu@Tolga-Air.home>
2024-03-17 17:18:04 +0300

    some fix

--- client/src/components/Author/Experiences.tsx
+++ client/src/components/Author/Experiences.tsx
@@ -1,3 +1,41 @@
+const experienced = [
+    {
+        title: "October 2021 - February 2022 | INTERN",
+        company: "PATH Product and Software House",
+        description: (
+            <p className="mb-4 font-normal text-base-content/50">
+                I worked on A&D Team and solved front-end problems. <br></br> I
+                have experienced a lot of things about team working,
+                responsibilities, communication skills <br></br>I have
+                experienced woocommerce and shopify e-commerce system and made
+                e-commerce websites.
+            </p>
+        ),
+    },
+    {
+        title: "June 2021 - July 2021 | INTERN",
+        company: "SNI",
+        description: (
+            <p className="mb-4 font-normal text-base-content/50">
+                Mobile App Development <br></br> I learned things about
+                fullstack developement and developed small music player mobile
+                project with nodejs, express, mongodb and flutter
+            </p>
+        ),
+    },
+    {
+        title: "August 2021 - August 2021 | Software Engineer",
+        company: "Mimix | ITU Cekirdek Startup",
+        description: (
+            <p className="mb-4 font-normal text-base-content/50">
+                Machine Learning Research with AWS SageMaker<br></br> Mobile App
+                Development with React Native, Amazon Web Services (AWS Route52,
+                Amplify)
+            </p>
+        ),
+    },
+];
+
 export default function Experiences() {
     return (
         <section className="px-8 lg:px-32 h-screen bg-base-200 flex flex-col justify-center text-base-content font-poppins">
@@ -6,50 +44,22 @@ export default function Experiences() {
             </h2>
             <br />
             <ol className="relative border-l border-base-content">
-                <li className="mb-10 ml-4">
-                    <div className="absolute w-3 h-3 bg-accent rounded-full mt-1.5 -left-1.5 border border-accent"></div>
-                    <time className="mb-1 text-xl font-bold leading-none text-primary">
-                        October 2021 - February 2022 | INTERN
-                    </time>
-                    <h3 className="text-lg font-bold">
-                        PATH Product and Software House
-                    </h3>
-                    <p className="mb-4 font-normal text-base-content/50">
-                        I worked on A&D Team and solved front-end problems.{" "}
-                        <br></br> I have experienced a lot of things about team
-                        working, responsibilities, communication skills{" "}
-                        <br></br>I have experienced woocommerce and shopify
-                        e-commerce system and made e-commerce websites.
-                    </p>
-                </li>
-
-                <li className="mb-10 ml-4">
-                    <div className="absolute w-3 h-3 bg-accent rounded-full mt-1.5 -left-1.5 border border-accent"></div>
-                    <time className="mb-1 text-md font-bold leading-none text-primary">
-                        June 2021 - July 2021 | INTERN
-                    </time>
-                    <h3 className="text-lg font-semibold">SNI</h3>
-                    <p className="mb-4 font-normal text-base-content/50">
-                        Mobile App Development <br></br> I learned things about
-                        fullstack developement and developed small music player
-                        mobile project with nodejs, express, mongodb and flutter
-                    </p>
-                </li>
-
-                <li className="mb-10 ml-4">
-                    <div className="absolute w-3 h-3 bg-accent rounded-full mt-1.5 -left-1.5 border border-accent"></div>
-                    <time className="mb-1 text-md font-bold leading-none text-primary">
-                        August 2021 - August 2021 | Junior Software Engineer
-                    </time>
-                    <h3 className="text-lg font-semibold">
-                        Mimix | ITU Cekirdek Startup
-                    </h3>
-                    <p className="mb-4 font-normal text-base-content/50">
-                        Machine Learning Research with AWS SageMaker<br></br>{" "}
-                        Mobile App Development with React Native, Amazon Web
-                        Services (AWS Route52, Amplify)
-                    </p>
-                </li>
+                {experienced.map((item) => {
+                    return (
+                        <>
+                            <li className="mb-10 ml-4">
+                                <div className="absolute w-3 h-3 bg-accent rounded-full mt-1.5 -left-1.5 border border-accent"></div>
+                                <time className="mb-1 text-md font-bold leading-none text-primary">
+                                    {item.title}
+                                </time>
+                                <h3 className="text-lg font-semibold">
+                                    {item.company}
+                                </h3>
+                                {item.description}
+                            </li>
+                        </>
+                    );
+                })}
             </ol>
         </section>
     );
--- client/src/components/Contact/index.tsx
+++ client/src/components/Contact/index.tsx
@@ -6,109 +6,113 @@ import { getError } from "../../utils/getError";
 import { ApiError } from "../../types/ApiError";
 
 const Contact = () => {
-  const { mutateAsync: createContact } = useCreateContactMutation();
+    const { mutateAsync: createContact } = useCreateContactMutation();
 
-  const [email, setEmail] = useState<string>("");
-  const [subject, setSubject] = useState<string>("");
-  const [message, setMessage] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [subject, setSubject] = useState<string>("");
+    const [message, setMessage] = useState<string>("");
 
-  const submitHandler = async (e: FormEvent) => {
-    e.preventDefault();
-    try {
-      await createContact({
-        email: email,
-        subject: subject,
-        message: message,
-      });
-      toast.success("Message sended!");
-    } catch (error) {
-      toast.error(getError(error as ApiError));
-    }
-  };
+    const submitHandler = async (e: FormEvent) => {
+        e.preventDefault();
+        try {
+            await createContact({
+                email: email,
+                subject: subject,
+                message: message,
+            });
+            toast.success("Message sended!");
+        } catch (error) {
+            toast.error(getError(error as ApiError));
+        }
+    };
 
-  return (
-    <>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={true}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
-      />
-      <Layout />
-      <section className="mt-14">
-        <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md font-poppins">
-          <h2 className="mb-4 text-4xl tracking-tight font-poppins text-center">
-            Contact Us
-          </h2>
-          <p className="mb-8 lg:mb-16 font-poppins text-center sm:text-xl">
-            Got a technical issue? Want to send feedback about a beta feature?
-            Need details about our Business plan? Let us know.
-          </p>
-          <form action="#" className="space-y-8">
-            <div>
-              <label htmlFor="email" className="block mb-2 text-sm font-medium">
-                Your email
-              </label>
-              <input
-                type="email"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="input input-bordered input-primary w-full border-primary"
-                placeholder="name@flowbite.com"
-                required
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="subject"
-                className="block mb-2 text-sm font-medium"
-              >
-                Subject
-              </label>
-              <input
-                type="text"
-                id="subject"
-                value={subject}
-                onChange={(e) => setSubject(e.target.value)}
-                className="input input-bordered input-primary w-full border-primary"
-                placeholder="Let us know how we can help you"
-                required
-              />
-            </div>
-            <div className="sm:col-span-2">
-              <label
-                htmlFor="message"
-                className="block mb-2 text-sm font-medium"
-              >
-                Your message
-              </label>
-              <textarea
-                id="message"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                className="textarea textarea-bordered textarea-primary textarea-md w-full border-primary"
-                placeholder="Leave a message..."
-              ></textarea>
-            </div>
-            <button
-              type="submit"
-              className="btn btn-primary btn-sm"
-              onClick={submitHandler}
-            >
-              Send message
-            </button>
-          </form>
-        </div>
-      </section>
-    </>
-  );
+    return (
+        <>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={true}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="colored"
+            />
+            <Layout />
+            <section className="mt-14">
+                <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md font-poppins">
+                    <h2 className="mb-4 text-4xl tracking-tight font-poppins text-center">
+                        Contact Us
+                    </h2>
+                    <p className="mb-8 lg:mb-16 font-poppins text-center sm:text-xl">
+                        Got a technical issue? Want to send feedback about a
+                        beta feature? Need details about our Business plan? Let
+                        us know.
+                    </p>
+                    <form action="#" className="space-y-8">
+                        <div>
+                            <label
+                                htmlFor="email"
+                                className="block mb-2 text-sm font-medium"
+                            >
+                                Your email
+                            </label>
+                            <input
+                                type="email"
+                                id="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                className="input input-bordered w-full "
+                                placeholder="name@flowbite.com"
+                                required
+                            />
+                        </div>
+                        <div>
+                            <label
+                                htmlFor="subject"
+                                className="block mb-2 text-sm font-medium"
+                            >
+                                Subject
+                            </label>
+                            <input
+                                type="text"
+                                id="subject"
+                                value={subject}
+                                onChange={(e) => setSubject(e.target.value)}
+                                className="input input-bordered w-full"
+                                placeholder="Let us know how we can help you"
+                                required
+                            />
+                        </div>
+                        <div className="sm:col-span-2">
+                            <label
+                                htmlFor="message"
+                                className="block mb-2 text-sm font-medium"
+                            >
+                                Your message
+                            </label>
+                            <textarea
+                                id="message"
+                                value={message}
+                                onChange={(e) => setMessage(e.target.value)}
+                                className="textarea textarea-bordered textarea-md w-full"
+                                placeholder="Leave a message..."
+                            ></textarea>
+                        </div>
+                        <button
+                            type="submit"
+                            className="btn btn-primary text-primary-content float-right"
+                            onClick={submitHandler}
+                        >
+                            Send message
+                        </button>
+                    </form>
+                </div>
+            </section>
+        </>
+    );
 };
 
 export default Contact;
