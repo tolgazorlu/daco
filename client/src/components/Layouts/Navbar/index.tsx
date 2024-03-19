dfae0a15e3c94a5d3a1591a510e6e64b13fa368d
Tolga Zorlu <tolgazorlu@Tolga-Air.mshome.net>
2024-03-19 13:06:42 +0300

    react never used problem fixed

--- client/src/components/Layouts/Navbar/index.tsx
+++ client/src/components/Layouts/Navbar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import { User } from "../../../contexts/User";
 
