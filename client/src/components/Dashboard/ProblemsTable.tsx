11a8ecb87a1f306d413dd3fc27dcaadc4d2448a2
Tolga Zorlu <tolgazorlu@Tolga-Air.mshome.net>
2024-03-19 14:03:21 +0300

    validateDomNesting error fixed

--- client/src/components/Dashboard/ProblemsTable.tsx
+++ client/src/components/Dashboard/ProblemsTable.tsx
@@ -31,35 +31,31 @@ const ProblemsTable = () => {
                 <div className="p-2 stats shadow-lg border border-base-300">
                     <div className="stat overflow-x-auto flex flex-col gap-4">
                         <div className="stat-title">All Problems</div>
-                        <table className="table table-xs font-poppins table-zebra">
-                            <div className="alert flex justify-center">
-                                <span className="loading loading-lg"></span>
-                            </div>
-                        </table>
+                        <div className="alert flex justify-center">
+                            <span className="loading loading-lg"></span>
+                        </div>
                     </div>
                 </div>
             ) : error ? (
                 <div className="p-2 stats shadow-lg border border-base-300">
                     <div className="stat overflow-x-auto flex flex-col gap-4">
                         <div className="stat-title">All Problems</div>
-                        <table className="table table-xs font-poppins table-zebra">
-                            <div className="alert alert-error">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="stroke-current shrink-0 h-6 w-6"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-                                    />
-                                </svg>
-                                <span>Error! Something went wrong.</span>
-                            </div>
-                        </table>
+                        <div className="alert alert-error">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="stroke-current shrink-0 h-6 w-6"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                                />
+                            </svg>
+                            <span>Error! Something went wrong.</span>
+                        </div>
                     </div>
                 </div>
             ) : (
