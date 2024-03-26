4d270204f0453f074e94ca1badd6d969f5ad2cfd
Tolga Zorlu <tolgazorlu@Tolga-Air.mshome.net>
2024-03-26 11:49:52 +0300

    add problem button added

--- client/src/components/Dashboard/Problems/index.tsx
+++ client/src/components/Dashboard/Problems/index.tsx
@@ -1,8 +1,47 @@
 import ProblemsTable from "../ProblemsTable";
 import Layout from "../../Layouts";
 import { Helmet } from "react-helmet-async";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useCreateProblemMutation } from "../../../hooks/problemHooks";
+import { toast } from "react-toastify";
+import { getError } from "../../../utils/getError";
+import { ApiError } from "../../../types/ApiError";
 
 const Problems = () => {
+    const { mutateAsync: createProblem } = useCreateProblemMutation();
+    const [createButtonLoading, setCreateButtonLoading] =
+        useState<boolean>(false);
+
+    const navigation = useNavigate();
+
+    const day = 1;
+    const title = "New Problem";
+    const level = "easy";
+    const description = "## New Problem";
+    const answer = "answer";
+    const slug = "slug";
+
+    const createProblemHandler = async () => {
+        try {
+            const data = await createProblem({
+                day: day,
+                title: title,
+                level: level,
+                description: description,
+                answer: answer,
+                slug: slug,
+            });
+            toast.success("Problem created!");
+            setCreateButtonLoading(true);
+            setInterval(() => {
+                navigation("/question/" + data.slug + "/edit");
+            }, 1000);
+        } catch (error) {
+            toast.error(getError(error as ApiError));
+            setCreateButtonLoading(false);
+        }
+    };
     return (
         <>
             <Helmet>
@@ -10,12 +49,16 @@ const Problems = () => {
             </Helmet>
             <Layout />
             <div className="px-4 py-8 lg:px-24 sm:ml-64 mt-16 flex flex-col gap-4">
-                <a
+                <button
                     className="btn btn-sm btn-primary text-primary-content rounded"
-                    href="/question/create"
+                    onClick={() => createProblemHandler()}
                 >
-                    Add Problem
-                </a>
+                    {createButtonLoading ? (
+                        <span className="loading loading-sm"></span>
+                    ) : (
+                        <span>Add New Problem</span>
+                    )}
+                </button>
                 <ProblemsTable />
             </div>
         </>
