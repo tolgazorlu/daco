68b5ff7331901f0fc8c3afde8eacb45f03fe63dc
Tolga Zorlu <tolgazorlu@Tolga-Air.mshome.net>
2024-04-13 13:36:53 +0300

    footer name changed daco to dacospace

--- client/src/components/Layouts/Footer/index.tsx
+++ client/src/components/Layouts/Footer/index.tsx
@@ -2,7 +2,9 @@ const Footer = () => {
     return (
         <footer className="footer footer-center p-10 bg-base-300 text-base-content">
             <aside>
-                <span className="font-bandal text-4xl text-primary">daco</span>
+                <span className="font-bandal text-4xl text-primary">
+                    dacospace
+                </span>
 
                 <p>Copyright © 2024 - All right reserved</p>
             </aside>
